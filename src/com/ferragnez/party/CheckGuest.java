b674956501f871d2599251a95abb5e68c67fe62d
Simone-Domenici <domenici.simone00@gmail.com>
2025-02-27 17:20:50 +0100

    Add guest list functionality to CheckGuest class

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,33 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
-        
+        String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        for (int i = 0; i < guests.length; i++){
+            System.out.println(guests[i]);
+        }
+        Scanner input = new Scanner(System.in);
+        String guestName = input.nextLine();
+        input.close();
+
+        boolean isInTheList = false;
+        int index = 0;
+
+        while (!isInTheList && index < guests.length) {
+            if (guests[index].equalsIgnoreCase(guestName)){
+                isInTheList = true;
+            } else {
+                index++;
+            }
+        }
+
+        if (isInTheList) {
+            System.out.println("Guest Found");
+        } else {
+           System.out.println("Guest NOT Found"); 
+        }
+
     }
 }
